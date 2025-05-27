c877289fb0ff3dd704b00f7773b6cc2b6b5c1bb4
Frederick Pringle <frederick.pringle@fpringle.com>
2025-05-27 09:31:58 +0200

    Package `effectful-opaleye`

---
+++ effectful-opaleye/src/Effectful/Opaleye.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Effectful.Opaleye
+  ( Opaleye (..)
+  , runSelect
+  , runInsert
+  , runDelete
+  , runUpdate
+  , runOpaleyeWithConnection
+  )
+where
+
+import Data.Profunctor.Product.Default
+import Effectful
+import Effectful.Dispatch.Dynamic
+import qualified Effectful.PostgreSQL.Connection as Conn
+import Effectful.TH
+import qualified Opaleye as O
+
+data Opaleye :: Effect where
+  RunSelect :: (Default O.FromFields fields haskells) => O.Select fields -> Opaleye m [haskells]
+  RunInsert :: O.Insert haskells -> Opaleye m haskells
+  RunDelete :: O.Delete haskells -> Opaleye m haskells
+  RunUpdate :: O.Update haskells -> Opaleye m haskells
+
+makeEffect ''Opaleye
+
+runOpaleyeWithConnection ::
+  (HasCallStack, Conn.WithConnection :> es, IOE :> es) =>
+  Eff (Opaleye : es) a ->
+  Eff es a
+runOpaleyeWithConnection = interpret $ \_ -> \case
+  RunSelect sel -> Conn.withConnection $ \conn -> liftIO $ O.runSelect conn sel
+  RunInsert sel -> Conn.withConnection $ \conn -> liftIO $ O.runInsert conn sel
+  RunDelete sel -> Conn.withConnection $ \conn -> liftIO $ O.runDelete conn sel
+  RunUpdate sel -> Conn.withConnection $ \conn -> liftIO $ O.runUpdate conn sel
